efeab5b02fb90e08d7de7b81003517b21b5aa980
Mikhail Kurenkov <Mikhail.Kurenkov@skoltech.ru>
2022-12-20 18:31:38 +0300

    make precondition and parametrization

--- swarm_nfomp/utils/math.py
+++ swarm_nfomp/utils/math.py
@@ -1,5 +1,23 @@
 import numpy as np
+import torch
 
 
 def wrap_angles(angles):
     return (angles + np.pi) % (2 * np.pi) - np.pi
+
+
+def interpolate_1d_pytorch(x: torch.Tensor, old_times: torch.Tensor, new_times: torch.Tensor) -> torch.Tensor:
+    upper_indices: torch.Tensor = torch.searchsorted(old_times, new_times)
+    lower_indices: torch.Tensor = upper_indices - 1
+    lower_indices = torch.where(lower_indices > 0, lower_indices, 0)
+    upper_indices = torch.where(upper_indices < x.shape[0], upper_indices, x.shape[0] - 1)
+    lower_old_times = torch.gather(old_times, 0, lower_indices)
+    upper_old_times = torch.gather(old_times, 0, upper_indices)
+    lower_x: torch.Tensor = x[lower_indices]
+    upper_x: torch.Tensor = x[upper_indices]
+    denominator = upper_old_times - lower_old_times
+    denominator = torch.where(torch.abs(denominator) > 1e-6, denominator, torch.full_like(denominator, 1e-6))
+    weights = (new_times - lower_old_times) / denominator
+    weights = weights[:, None]
+    interpolated_values = (1 - weights) * lower_x + weights * upper_x
+    return interpolated_values
